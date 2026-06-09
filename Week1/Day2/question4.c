33fe0735c18e1ceb4b2bc51ed0dbab6a2691da20
Shourya922 <shourya.singh2025@glbajajgroup.org>
2026-06-09 20:37:53 +0530

    Update question4.c

--- Week1/Day2/question4.c
+++ Week1/Day2/question4.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main() {
+    int n, original, rev = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    original = n;
+
+    while (n != 0) {
+        rev = rev * 10 + n % 10;
+        n /= 10;
+    }
+
+    if (original == rev)
+        printf("%d is a Palindrome Number", original);
+    else
+        printf("%d is not a Palindrome Number", original);
+
+    return 0;
+}
