874d3609d5c03aba0915973f08fbfcc50cd0ec5b
Shourya922 <shourya.singh2025@glbajajgroup.org>
2026-06-09 20:35:31 +0530

    Update Question3.c

--- Week1/Day2/Question3.c
+++ Week1/Day2/Question3.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+int main() {
+    int n, product =1; 
+    
+    printf("enter no");
+    scanf("%d" ,&n);
+    while (n !=0){
+        rev =rev *10+n%10;
+        n/=10
+    }
+    printf("product of digits = %d" , product);
+    return 0;
+}
 
