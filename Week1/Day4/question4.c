a5eb3d3b64746fa0d30bc8a3d336e84856231586
Shourya922 <shourya.singh2025@glbajajgroup.org>
2026-06-09 20:49:40 +0530

    Update question4.c

--- Week1/Day4/question4.c
+++ Week1/Day4/question4.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int main() {
+    int n, original, rem;
+    int sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    original = n;
+
+    while(n != 0) {
+        rem = n % 10;
+        sum += rem * rem * rem;
+        n /= 10;
+    }
+
+    if(sum == original)
+        printf("%d is an Armstrong Number", original);
+    else
+        printf("%d is not an Armstrong Number", original);
+
+    return 0;
+}
