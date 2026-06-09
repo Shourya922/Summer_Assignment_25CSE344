c8e7d53f228eec7e6f422c049ce186920dc7061f
Shourya922 <shourya.singh2025@glbajajgroup.org>
2026-06-09 20:44:41 +0530

    Update question4.c

--- Week1/Day3/question4.c
+++ Week1/Day3/question4.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+int main() {
+    int a, b, temp, gcd, lcm;
 
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    int x = a, y = b;
+
+    while (y != 0) {
+        temp = y;
+        y = x % y;
+        x = temp;
+    }
+
+    gcd = x;
+    lcm = (a * b) / gcd;
+
+    printf("LCM = %d", lcm);
+
+    return 0;
+}
