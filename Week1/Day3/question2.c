4fbc85f3ecdd23d367caef8837330b723c50f84e
Shourya922 <shourya.singh2025@glbajajgroup.org>
2026-06-09 20:43:24 +0530

    Update question2.c

--- Week1/Day3/question2.c
+++ Week1/Day3/question2.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+int main() {
+    int start, end, i, j, isPrime;
 
+    printf("Enter start and end: ");
+    scanf("%d %d", &start, &end);
+
+    printf("Prime numbers are:\n");
+
+    for (i = start; i <= end; i++) {
+        if (i <= 1)
+            continue;
+
+        isPrime = 1;
+
+        for (j = 2; j <= i / 2; j++) {
+            if (i % j == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+
+        if (isPrime)
+            printf("%d ", i);
+    }
+
+    return 0;
+}
