cdc15cc9240f142db8eb1162005e37832f3b65c1
Shourya922 <shourya.singh2025@glbajajgroup.org>
2026-06-17 07:41:12 +0530

    Update Question3.c

--- Week1/Day14/Question3.c
+++ Week1/Day14/Question3.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <limits.h>
 
+int main() {
+    int n, i;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int largest = INT_MIN, second = INT_MIN;
+
+    for(i = 0; i < n; i++) {
+        if(arr[i] > largest) {
+            second = largest;
+            largest = arr[i];
+        }
+        else if(arr[i] > second && arr[i] != largest) {
+            second = arr[i];
+        }
+    }
+
+    printf("Second Largest Element = %d", second);
+
+    return 0;
+}
