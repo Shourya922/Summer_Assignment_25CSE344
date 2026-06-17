89760ad2666c1df3373b1859f8505e24b19e5f73
Shourya922 <shourya.singh2025@glbajajgroup.org>
2026-06-17 07:41:40 +0530

    Update Question4.c

--- Week1/Day14/Question4.c
+++ Week1/Day14/Question4.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+int main() {
+    int n, i, j, found = 0;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Duplicate elements are: ");
+
+    for(i = 0; i < n; i++) {
+        for(j = i + 1; j < n; j++) {
+            if(arr[i] == arr[j]) {
+                printf("%d ", arr[i]);
+                found = 1;
+                break;
+            }
+        }
+    }
+
+    if(found == 0)
+        printf("No duplicates found");
+
+    return 0;
+}
