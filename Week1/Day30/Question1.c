86c317f81b34b55f28ee18edd4d9dcdb2141ff57
Shourya922 <shourya.singh2025@glbajajgroup.org>
2026-06-27 22:01:16 +0530

    Update Question1.c

--- Week1/Day30/Question1.c
+++ Week1/Day30/Question1.c
@@ -1,1 +1,75 @@
+#include <stdio.h>
+#include <string.h>
 
+struct Student {
+    int roll;
+    char name[50];
+    float marks;
+};
+
+int main() {
+    struct Student s[100];
+    int n = 0, choice, i, roll, found;
+
+    while (1) {
+        printf("\n===== Student Record System =====\n");
+        printf("1. Add Student\n");
+        printf("2. Display Students\n");
+        printf("3. Search Student\n");
+        printf("4. Exit\n");
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+
+        switch(choice) {
+            case 1:
+                printf("Enter Roll Number: ");
+                scanf("%d", &s[n].roll);
+
+                printf("Enter Name: ");
+                scanf(" %[^\n]", s[n].name);
+
+                printf("Enter Marks: ");
+                scanf("%f", &s[n].marks);
+
+                n++;
+                printf("Student Added Successfully!\n");
+                break;
+
+            case 2:
+                printf("\nStudent Records\n");
+                printf("-------------------------------\n");
+                for(i = 0; i < n; i++) {
+                    printf("Roll: %d\n", s[i].roll);
+                    printf("Name: %s\n", s[i].name);
+                    printf("Marks: %.2f\n\n", s[i].marks);
+                }
+                break;
+
+            case 3:
+                printf("Enter Roll Number to Search: ");
+                scanf("%d", &roll);
+
+                found = 0;
+                for(i = 0; i < n; i++) {
+                    if(s[i].roll == roll) {
+                        printf("Student Found!\n");
+                        printf("Roll: %d\n", s[i].roll);
+                        printf("Name: %s\n", s[i].name);
+                        printf("Marks: %.2f\n", s[i].marks);
+                        found = 1;
+                        break;
+                    }
+                }
+
+                if(!found)
+                    printf("Student Not Found!\n");
+                break;
+
+            case 4:
+                return 0;
+
+            default:
+                printf("Invalid Choice!\n");
+        }
+    }
+}
