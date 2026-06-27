813e64f61bb526ab8180067af234672b2da2d9a2
Shourya922 <shourya.singh2025@glbajajgroup.org>
2026-06-27 20:38:32 +0530

    Update Questiuon1.c

--- Week1/Day28/Questiuon1.c
+++ Week1/Day28/Questiuon1.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+struct Book {
+    int id;
+    char title[50];
+    char author[50];
+};
+
+int main() {
+    struct Book b;
+
+    printf("Enter Book ID: ");
+    scanf("%d", &b.id);
+
+    printf("Enter Book Title: ");
+    scanf(" %[^\n]", b.title);
+
+    printf("Enter Author Name: ");
+    scanf(" %[^\n]", b.author);
+
+    printf("\n----- Book Details -----\n");
+    printf("Book ID : %d\n", b.id);
+    printf("Title   : %s\n", b.title);
+    printf("Author  : %s\n", b.author);
+
+    return 0;
+}
