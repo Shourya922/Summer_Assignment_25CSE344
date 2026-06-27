a3cb2103953e7ec1e6858cfcaded83c3092e0976
Shourya922 <shourya.singh2025@glbajajgroup.org>
2026-06-27 20:39:34 +0530

    Update Question3.c

--- Week1/Day28/Question3.c
+++ Week1/Day28/Question3.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main() {
+    int seats = 50;
+    int book;
+
+    printf("Available Seats: %d\n", seats);
+
+    printf("Enter Number of Seats to Book: ");
+    scanf("%d", &book);
+
+    if(book <= seats) {
+        seats -= book;
+        printf("Booking Successful!\n");
+        printf("Remaining Seats: %d\n", seats);
+    } else {
+        printf("Not Enough Seats Available!\n");
+    }
+
+    return 0;
+}
